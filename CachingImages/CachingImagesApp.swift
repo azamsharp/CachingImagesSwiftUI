b3ca633fd799f4bef1378286897758ed7598920a
Mohammad Azam <azamsharp@gmail.com>
2022-01-03 13:59:55 -0600

    Initial Commit

---
+++ CachingImages/CachingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingImagesApp.swift
+//  CachingImages
+//
+//  Created by Mohammad Azam on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CachingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
